ccc116221422bcbbed9445c17393fcdaf630f4c1
Ginger <154593833@qq.com>
2020-10-08 16:08:43 +0800

    #修复：新增账户界面、流水界面

--- app/src/main/java/com/xuexiang/cdaccount/utils/DemoDataProvider.java
+++ app/src/main/java/com/xuexiang/cdaccount/utils/DemoDataProvider.java
@@ -28,6 +28,8 @@ import com.xuexiang.xui.utils.ResUtils;
 import com.xuexiang.xui.widget.banner.widget.banner.BannerItem;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 /**
@@ -117,4 +119,7 @@ public class DemoDataProvider {
         return list;
     }
 
+    public static Collection<String> getDemoData1() {
+        return Arrays.asList("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18");
+    }
 }
